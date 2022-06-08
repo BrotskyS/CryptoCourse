82d92b55bc2cc433fb8e084c2df7ab3afe7b283e
BrotskyS <sergiypik.work@gmail.com>
2022-06-08 16:31:36 +0300

    Initial Commit

---
+++ CryptoCourse/CryptoCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCourseApp.swift
+//  CryptoCourse
+//
+//  Created by Sergiy Brotsky on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
